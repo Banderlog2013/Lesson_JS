32a4b8ce092209bc3bececeb69980deec968b25f
Pavel <banderlog2013@yandex.ru>
2022-03-29 11:08:01 +0300

    first commit

---
+++ 8. Интерполяция (ES6)/script.js
@@ -0,0 +1,10 @@
+"use strict"
+
+const category = 'toys';
+console.log(`https://someurl.com/${category}/5`);
+
+const user = 'Вася';
+alert(`Привет, ${user}`);
+
+//Интерполяция способ соединения строк через вставку значений переменных с помощию 
+// бектиков, знака $ и {}. Привет, ${user}` 
